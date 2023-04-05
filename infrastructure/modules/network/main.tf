359b12f01da2162a5d2cc90414fc5579c09110f6
Dylan Felkers <dylan.felkers@batsamayi.com>
2023-04-05 09:44:07 +0200

    Vpc cird range as Variable added

--- infrastructure/modules/network/main.tf
+++ infrastructure/modules/network/main.tf
@@ -1,3 +1,3 @@
 resource "aws_vpc" "network" {
-    cidr_block = "10.0.0.0/16"
+    cidr_block = var.vpc_cidr
 }
---
+++ infrastructure/modules/network/variables.tf
@@ -0,0 +1,7 @@
+variable "vpc_cidr" {
+
+    description = "Cidr range for VPC"
+    type = string
+    default = "10.0.0.0/16"
+  
+}
