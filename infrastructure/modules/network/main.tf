bed9013095404144a6c37290ddd515eb6e8fab05
Mxolisi Tshezi <71215268+Mxolisi-Tshezi@users.noreply.github.com>
2023-04-05 13:51:10 +0200

    added flow logs

--- infrastructure/modules/network/main.tf
+++ infrastructure/modules/network/main.tf
@@ -59,3 +59,57 @@ resource "aws_subnet" "private_subnet-2" {
     Name =var.private_subnet-2_name
   }
 }
+
+
+# create vpc flow logs
+
+resource "aws_flow_log" "loggs" {
+  iam_role_arn    = aws_iam_role.flowlog.arn
+  log_destination = aws_cloudwatch_log_group.alerts.arn
+  traffic_type    = "ALL"
+  vpc_id          = aws_vpc.vpc.id
+}
+
+resource "aws_cloudwatch_log_group" "alerts" {
+  name = var.aws_cloudwatch_log
+}
+
+data "aws_iam_policy_document" "assume_role" {
+  statement {
+    effect = "Allow"
+
+    principals {
+      type        = "Service"
+      identifiers = ["vpc-flow-logs.amazonaws.com"]
+    }
+
+    actions = ["sts:AssumeRole"]
+  }
+}
+
+resource "aws_iam_role" "flowlog" {
+  name               =var.aws_iam_role_flow_log
+  assume_role_policy = data.aws_iam_policy_document.assume_role.json
+}
+
+data "aws_iam_policy_document" "flowlog" {
+  statement {
+    effect = "Allow"
+
+    actions = [
+      "logs:CreateLogGroup",
+      "logs:CreateLogStream",
+      "logs:PutLogEvents",
+      "logs:DescribeLogGroups",
+      "logs:DescribeLogStreams",
+    ]
+
+    resources = ["*"]
+  }
+}
+
+resource "aws_iam_role_policy" "flowlog" {
+  name   = "flowlog"
+  role   = aws_iam_role.flowlog.id
+  policy = data.aws_iam_policy_document.flowlog.json
+}
